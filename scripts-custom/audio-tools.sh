a718331e95c2a936a72029518581e3412e6146b4
Ben Johnson <ben@indiehd.com>
2021-02-18 10:47:09 -0500

    fix: Issue with audio processing tool script and obsoleted MariaDB directive

--- scripts-custom/audio-tools.sh
+++ scripts-custom/audio-tools.sh
@@ -8,10 +8,10 @@ echo 'Installing audio processing tools...'
 DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confnew" install -yq sox libsox3 libsox-dev libsox-fmt-all flac lame ffmpeg atomicparsley python3-setuptools imagemagick
 
 # Mutagen requires Python version > 2!
-update-alternatives --install /usr/bin/python python /usr/bin/python3.6 1
+update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 
 git clone https://github.com/quodlibet/mutagen.git /tmp/mutagen
 cd /tmp/mutagen
 git checkout -b release-1.42.0
 ./setup.py build
-sudo ./setup.py install
+./setup.py install
